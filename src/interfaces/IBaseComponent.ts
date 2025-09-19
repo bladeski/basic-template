a1d66a246b949a516e6ae8d27b65d16caf574646
Jonathan Blades <jonoblades@gmail.com>
2025-09-19 16:19:57 +0100

    Create IBaseComponent.ts

---
+++ src/interfaces/IBaseComponent.ts
@@ -0,0 +1,43 @@
+export type PropTypes = Record<string, any>;
+
+export interface IBaseComponent<
+  TProps extends PropTypes = {},
+  TEvents = {}
+> extends HTMLElement {
+  /** Strongly typed props */
+  props: TProps;
+
+  /** Optional ID of a <template> element to render */
+  templateId?: string;
+
+  /**
+   * Called when the component is added to the DOM.
+   */
+  connectedCallback(): void;
+
+  /**
+   * Called when the component is removed from the DOM.
+   */
+  disconnectedCallback(): void;
+
+  /**
+   * Called when an observed attribute changes.
+   */
+  attributeChangedCallback(
+    name: keyof TProps & string,
+    oldValue: string | null,
+    newValue: string | null
+  ): void;
+
+  /**
+   * Render method for updating the DOM.
+   * If `templateId` is set, the base implementation will
+   * clone that template into the shadow DOM.
+   */
+  render(): void;
+
+  /**
+   * Dispatch a typed custom event.
+   */
+  emit<K extends keyof TEvents & string>(eventName: K, detail: TEvents[K]): void;
+}
